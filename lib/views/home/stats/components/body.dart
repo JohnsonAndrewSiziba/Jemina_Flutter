86fab55deeaa9ceb4ae363b4308b296edac05bd3
Ender <siziba.uz@outlook.com>
2022-03-21 15:06:29 +0200

    Monday afternoon

---
+++ lib/views/home/stats/components/body.dart
@@ -0,0 +1,246 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:jemina_capital/data/constants/theme_colors.dart';
+
+class StatsBody extends StatefulWidget {
+  StatsBody({Key? key}) : super(key: key);
+
+  @override
+  State<StatsBody> createState() => _StatsBodyState();
+}
+
+class _StatsBodyState extends State<StatsBody> {
+  List<String> categories = [
+    "Price Sheets",
+    "Market Stats.",
+  ];
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: techBlue,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(11.0),
+              bottomRight: Radius.circular(11.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 8),
+                blurRadius: 24,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: kDefaultPadding - 10.0),
+            child: SizedBox(
+              height: 25,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) => buildCategory(index),
+              ),
+            ),
+          ),
+        ),
+        MainBody(),
+      ],
+    );
+  }
+
+  Widget buildCategory(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              categories[index],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: selectedIndex == index
+                    ? Colors.white
+                    : Colors.white.withOpacity(0.7),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: kDefaultPadding / 4), //top padding 5
+              height: 2,
+              width: 30,
+              color: selectedIndex == index ? complement : Colors.transparent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainBody extends StatelessWidget {
+  const MainBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+            horizontal: kDefaultPadding - 10.0, vertical: kDefaultPadding),
+        physics: ClampingScrollPhysics(),
+        child: Column(
+          children: <Widget>[
+            _reportCell(
+              title: 'Savings Account',
+              deposit: '\$5,450',
+              rate: '+3,50%',
+              progress: 25,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            _reportCell(
+              title: 'Financial Cushion',
+              deposit: '\$12,350',
+              rate: '+3,50%',
+              progress: 50,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _reportCell({
+    required String title,
+    required String deposit,
+    required String rate,
+    required int progress,
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(color: techBlue.withOpacity(0.1), width: 2),
+        boxShadow: [
+          BoxShadow(
+            color: scaffoldBackgroundColor,
+            offset: Offset(0, 8),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(
+                  value: progress / 100,
+                  strokeWidth: 6,
+                  backgroundColor: techBlue.withOpacity(0.2),
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    techBlue,
+                  ),
+                ),
+              ),
+              Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  progress.toString() + '%',
+                  style: TextStyle(
+                    color: techBlue,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: <Widget>[
+                  _reportInnerCell(
+                    title: 'Deposit',
+                    value: deposit,
+                  ),
+                  SizedBox(
+                    width: 24,
+                  ),
+                  _reportInnerCell(
+                    title: 'Rate',
+                    value: rate,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _reportInnerCell({required String title, required String value}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(
+            color: techBlue,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(
+          value,
+          style: TextStyle(
+            color: techBlue,
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/home/stats/stats_page.dart
+++ lib/views/home/stats/stats_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:jemina_capital/views/home/stats/components/body.dart';
 
 import '../../../data/constants/theme_colors.dart';
 import '../../../widgets/go_to_profile.dart';
@@ -21,216 +22,20 @@ class _StatsPageState extends State<StatsPage> {
   static Color secondaryColor = complement;
   bool _isIncome = false;
 
-  List<String> categories = [
-    "Price Sheets",
-    "Market Stats.",
-  ];
-  int selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: scaffoldBackgroundColor,
       appBar: buildAppBar(context),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: techBlue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(11.0),
-                bottomRight: Radius.circular(11.0),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 8),
-                  blurRadius: 24,
-                  color: Colors.white,
-                ),
-              ],
-            ),
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: kDefaultPadding - 10.0),
-              child: SizedBox(
-                height: 25,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) => buildCategory(index),
-                ),
-              ),
-            ),
-          ),
-          _mainBody(),
-        ],
-      ),
+      body: StatsBody(),
     );
   }
 
   /// Main Body
-  Expanded _mainBody() {
-    return Expanded(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-            horizontal: kDefaultPadding, vertical: kDefaultPadding),
-        physics: ClampingScrollPhysics(),
-        child: Column(
-          children: <Widget>[
-            _reportCell(
-              isSavings: true,
-              title: 'Savings Account',
-              deposit: '\$5,450',
-              rate: '+3,50%',
-              progress: 25,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            _reportCell(
-              isSavings: false,
-              title: 'Financial Cushion',
-              deposit: '\$12,350',
-              rate: '+3,50%',
-              progress: 50,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   /// Report Cell
-  Container _reportCell({
-    required bool isSavings,
-    required String title,
-    required String deposit,
-    required String rate,
-    required int progress,
-  }) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16),
-      decoration: BoxDecoration(
-        color: grayBackground,
-        borderRadius: BorderRadius.circular(10.0),
-        border: isSavings
-            ? Border.all(color: primaryColor.withOpacity(0.1), width: 2)
-            : null,
-        boxShadow: isSavings
-            ? []
-            : [
-                BoxShadow(
-                  color: secondaryColor.withOpacity(0.4),
-                  offset: Offset(0, 8),
-                  blurRadius: 10,
-                ),
-              ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(
-                  value: progress / 100,
-                  strokeWidth: 6,
-                  backgroundColor: isSavings
-                      ? primaryColor.withOpacity(0.2)
-                      : primaryColor.withOpacity(0.1),
-                  valueColor: AlwaysStoppedAnimation<Color>(
-                      isSavings ? secondaryColor : Colors.white),
-                ),
-              ),
-              Container(
-                width: 60,
-                height: 60,
-                alignment: Alignment.center,
-                child: Text(
-                  progress.toString() + '%',
-                  style: TextStyle(
-                    color: isSavings ? primaryColor : Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: 30,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                title,
-                style: TextStyle(
-                  color: isSavings ? primaryColor : Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Row(
-                children: <Widget>[
-                  _reportInnerCell(
-                    isSavings: isSavings,
-                    title: 'Deposit',
-                    value: deposit,
-                  ),
-                  SizedBox(
-                    width: 24,
-                  ),
-                  _reportInnerCell(
-                    isSavings: isSavings,
-                    title: 'Rate',
-                    value: rate,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
 
   /// Report Inner Cell
-  Column _reportInnerCell(
-      {required bool isSavings, required String title, required String value}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          title,
-          style: TextStyle(
-            color: isSavings ? primaryColor.withOpacity(0.5) : Colors.white,
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          value,
-          style: TextStyle(
-            color: isSavings ? Colors.black87 : Colors.white,
-            fontSize: 14,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ],
-    );
-  }
 
   AppBar buildAppBar(BuildContext context) {
     return AppBar(
@@ -249,39 +54,6 @@ class _StatsPageState extends State<StatsPage> {
       actions: [
         GotoProfile(),
       ],
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              categories[index],
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: selectedIndex == index
-                    ? Colors.white
-                    : Colors.white.withOpacity(0.7),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: kDefaultPadding / 4), //top padding 5
-              height: 2,
-              width: 30,
-              color: selectedIndex == index ? complement : Colors.transparent,
-            )
-          ],
-        ),
-      ),
     );
   }
 }
