f689cbfa12d74797a0879840207e2b6b95c41d10
Ender <siziba.uz@outlook.com>
2022-03-25 09:55:39 +0200

    Api routes

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 class Controller{
   Routes routes = Routes();
 
-
   void postRequest(){
 
   }
--- lib/data/constants/api_routes.dart
+++ lib/data/constants/api_routes.dart
@@ -3,10 +3,10 @@ class Routes{
   static const String apiHome = "http://127.0.0.1:8000/api";
 
   // AUTH ROUTES
-  static const loginRoute = "$apiHome/mobile/login";
-  static const logoutRoute = "$apiHome/mobile/logout";
-  static const registerRoute = "$apiHome/mobile/register";
-  static const confirmEmailRoute = "$apiHome/mobile/confirm-email";
-  static const sendEmailNotificationRoute = "$apiHome/mobile/send-email-confirmation";
+  static Uri loginRoute =  Uri.parse("$apiHome/mobile/login");
+  static Uri logoutRoute = Uri.parse("$apiHome/mobile/logout");
+  static Uri registerRoute = Uri.parse("$apiHome/mobile/register");
+  static Uri confirmEmailRoute = Uri.parse("$apiHome/mobile/confirm-email");
+  static Uri sendEmailNotificationRoute = Uri.parse("$apiHome/mobile/send-email-confirmation");
 
 }
